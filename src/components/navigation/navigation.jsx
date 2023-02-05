6d439b857aecd387d3115a04854a1c9bf6f44934
akshkin <107682289+akshkin@users.noreply.github.com>
2023-02-05 12:25:59 +0100

    fixed animation for navbar and changed description for shopzone

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,36 +1,24 @@
-import { useMemo, useState } from "react";
+import { useCallback } from "react";
 import NavLinks from "./nav-links.component";
 import "./navigation.style.scss";
 
 function Navigation() {
-  const [isOpen, setIsOpen] = useState(false);
-  //const [openClass, setOpenClass] = useState("close")
-  let openClass = "";
+  const toggleNavigation = useCallback(() => {
+    const navbar = document.querySelector(".nav-links-container");
 
-  function handleToggleBtn() {
-    setIsOpen((prevOpen) => !prevOpen);
-  }
-  openClass = isOpen ? "open" : "close";
+    navbar.classList.toggle("open");
+  }, []);
 
   return (
     <header>
-      <nav className="mobile">
-        <img className="logo" src="/images/logo.png" alt="logo" />
-        <button className="toggle-btn" onClick={handleToggleBtn}>
-          &#9776;
-        </button>
-        {isOpen && (
-          <NavLinks
-            openClass={openClass}
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-          />
-        )}
-      </nav>
-
-      <nav className="desktop">
-        <img className="logo" src="/images/logo.png" alt="logo" />
-        <NavLinks />
+      <nav>
+        <div className="logo-container">
+          <img className="logo" src="/images/logo.png" alt="logo" />
+          <button className="toggle-btn" onClick={toggleNavigation}>
+            &#9776;
+          </button>
+        </div>
+        <NavLinks toggleNavigation={toggleNavigation} />
       </nav>
     </header>
   );
