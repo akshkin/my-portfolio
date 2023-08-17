2cee413b5354efe0e31b69d3c2e8911f02e0a55a
akshkin <107682289+akshkin@users.noreply.github.com>
2023-08-17 14:39:23 +0200

    Change font size of h1

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,8 +3,10 @@ import AnimatedLetters from "../animated-letters/animated-letters.component";
 
 function Hero() {
   const letterClass = "text-animate";
-  const name = "Akshaya  Kini";
+  const name = "Akshaya";
+  const surname = "Kini";
   const nameArray = name.split("");
+  const surnameArray = surname.split("");
   const jobTitle = "Web developer with a focus on frontend";
   const jobTitleArray = jobTitle.split("");
 
@@ -20,6 +22,14 @@ function Hero() {
             strArray={nameArray}
             index={15}
           />
+          &nbsp;
+          <span className="accent surname">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={surnameArray}
+              index={25}
+            />
+          </span>
         </span>
       </h1>
       <p className="hero-subtitle">Web developer with a focus on frontend</p>
