d298074d93c17c23690992ca6c914248322b8256
Akshaya <akshaya.kini12@gmail.com>
2026-05-05 21:45:18 +0200

    Migrate from CRA to vite

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,45 +5,45 @@ import Education from "./education/education";
 import Skills from "./education/skills";
 import Projects from "./projects/projects";
 import Contact from "./contact/contact";
-import { ReactComponent as DownloadIcon } from "../assets/svgs/download.svg";
+import DownloadIcon from "../assets/svgs/download.svg?react";
 
 function Home() {
-  return (
-    <div>
-      <main>
-        <div className="hero container">
-          <Hero />
-        </div>
-        <div className="about container" id="about">
-          <About />
-        </div>
-        <div className="education container" id="education">
-          <div className="inner">
-            <Education />
-          </div>
+	return (
+		<div>
+			<main>
+				<div className="hero container">
+					<Hero />
+				</div>
+				<div className="about container" id="about">
+					<About />
+				</div>
+				<div className="education container" id="education">
+					<div className="inner">
+						<Education />
+					</div>
 
-          <div className="inner">
-            <Skills />
-          </div>
+					<div className="inner">
+						<Skills />
+					</div>
 
-          {/* <div className="download-container">
+					{/* <div className="download-container">
             <a className="download" href="/Akshaya_Kini_Resume.pdf" download>
               Download Resume <DownloadIcon />
             </a>
           </div> */}
-        </div>
+				</div>
 
-        <div id="projects">
-          <h2 className="projects-title">MY PROJECTS</h2>
-          <Projects />
-        </div>
+				<div id="projects">
+					<h2 className="projects-title">MY PROJECTS</h2>
+					<Projects />
+				</div>
 
-        <div className="contact container" id="contact">
-          <Contact />
-        </div>
-      </main>
-    </div>
-  );
+				<div className="contact container" id="contact">
+					<Contact />
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default Home;
