3dce1567e196f131ea775b9319b0a363c019b481
Akshaya <akshaya.kini12@gmail.com>
2026-09-10 08:56:13 +0200

    Move blogs inside locale directory and create a back button component

---
+++ app/[locale]/blogs/[slug]/page.jsx
@@ -0,0 +1,54 @@
+import BackButton from "@/components/BackButton";
+import fs from "fs";
+import path from "path";
+import ReactMarkdown from "react-markdown";
+
+export async function generateMetadata({ params, searchParams }, parent) {
+	// read route params
+	const { slug } = await params;
+
+	return {
+		title: slug,
+	};
+}
+
+export default async function BlogPost({ params }) {
+	const { slug } = await params;
+	const filePath = path.join(process.cwd(), "content/blog", `${slug}.mdx`);
+
+	const fileContent = fs.readFileSync(decodeURIComponent(filePath), "utf8");
+
+	return (
+		<main className="p-6 max-w-3xl mx-auto mt-20">
+			<BackButton />
+			<article
+				className="mt-4 prose prose-invert prose-lg prose-headings:text-white prose-h1:text-5xl
+                prose-p:text-gray-300
+                prose-code:text-pink-400
+                prose-pre:bg-zinc-900
+                prose-headings:text-left
+                prose-pre:rounded-xl
+                prose-pre:p-4
+                prose-code:before:hidden
+                prose-code:after:hidden
+                prose-headings:tracking-tight
+                prose-img:rounded-xl
+                "
+			>
+				<ReactMarkdown
+				// components={{
+				// 	code({ children }) {
+				// 		return (
+				// 			<pre className="rounded-lg p-4 overflow-auto">
+				// 				<code>{children}</code>
+				// 			</pre>
+				// 		);
+				// 	},
+				// }}
+				>
+					{fileContent}
+				</ReactMarkdown>
+			</article>
+		</main>
+	);
+}
